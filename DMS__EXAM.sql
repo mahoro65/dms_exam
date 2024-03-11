80b69dc84fff8de21e8b5150274cfda8ac0a7876
mahoro65 <143531273+mahoro65@users.noreply.github.com>
2024-03-11 06:49:46 -0700

    Add files via upload

---
+++ DMS__EXAM.sql
@@ -0,0 +1,42 @@
+--CREATE TABLE students(
+	--Reg_no INT, 
+	--First_Name TEXT,
+	--Last_Name TEXT,
+	--Course TEXT,
+	--Gender TEXT,
+	--D_O_B INTEGER
+--;	
+--SELECT * FROM students;
+
+--INSERT INTO students(Reg_no, First_Name,Last_Name,Course,Gender,D_O_B)
+--VALUES(1/2016,'John','Mutuku','DCS','Male',13_6_1990),
+	--(2/2016,'Steve','KipKorir','DCS','Male',13_3_1985),
+	--(3/2016,'Suzan','Mutua','CIT','Female',19_11_1986),
+	--(4/2017,'Steve','Kingori','DBIT','Male',1_3_1978);
+
+--SELECT * FROM students;
+
+--i,SELECT reg_no, last_name,first_name FROM students
+
+
+--ii,
+
+--iv,
+--SELECT COUNT(*) FROM students;
+
+
+--v,
+--SELECT * FROM students WHERE Gender = 'Male' AND EXTRACT FROM (D_O_B) BETWEEN 1980 AND 1989;
+
+
+--vi
+--UPDATE students
+--SET last_Name='Mwangi'
+--WHERE Reg_no=4;
+--SELECT * FROM students;
+
+--vii
+-- Delete all student records from the table
+-- DELETE FROM students;
+
+
